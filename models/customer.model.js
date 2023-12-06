e17a59f7d9a952bba5382ff7638b6fc0a258222a
21127334 <lvlam21@clc.fitus.edu.vn>
2023-12-06 20:31:15 +0700

    feat: update time fields on customers, change users -> customers tab

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,51 +1,52 @@
-const db = require('./db');
-const schema = 'customers';
+const db = require("./db");
+const schema = "customers";
 
 const CustomerModel = {
-	add: async (firstname, lastname, email, pw) => {
-		try {
-			const customer = await db.get(schema, 'email', email);
-			if (customer) {
-				return {
-					status: false,
-					msg: `User with ${email} email is already exists`,
-				};
-			}
-			await db.add(schema, {
-				first_name: firstname,
-				last_name: lastname,
-				email: email,
-				password: pw,
-			});
-			return { status: true, msg: `User create successfully` };
-		} catch (err) {
-			console.error(err);
-		}
-	},
-	get: async (email) => {
-		try {
-			const customer = await db.get(schema, 'email', email);
-			return customer;
-		} catch (err) {
-			console.error(err);
-		}
-	},
-	getAll: async () => {
-		try {
-			const customers = await db.getAll(schema);
-			return customers;
-		} catch (err) {
-			console.error(err);
-		}
-	},
-	update: async (email, updateData) => {
-		try {
-			const result = await db.update(schema, 'email', email, updateData);
-			return result;
-		} catch (err) {
-			console.error(err);
-		}
-	},
+  add: async (firstname, lastname, email, pw) => {
+    try {
+      const customer = await db.get(schema, "email", email);
+      if (customer) {
+        return {
+          status: false,
+          msg: `User with ${email} email is already exists`,
+        };
+      }
+      await db.add(schema, {
+        first_name: firstname,
+        last_name: lastname,
+        email: email,
+        password: pw,
+      });
+      return { status: true, msg: `User create successfully` };
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  get: async (email) => {
+    try {
+      const customer = await db.get(schema, "email", email);
+      return customer;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  getAll: async () => {
+    try {
+      const customers = await db.getAll(schema);
+      return customers;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  update: async (email, updateData) => {
+    try {
+      updateData.last_updated = new Date();
+      const result = await db.update(schema, "email", email, updateData);
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  },
 };
 
 module.exports = CustomerModel;
