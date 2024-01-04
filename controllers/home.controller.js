39e97ced9657030b910fd4986610bb5c151e7431
21127334 <lvlam21@clc.fitus.edu.vn>
2024-01-05 01:27:22 +0700

    feat: Update home page to show user balance if logged in

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,42 +1,43 @@
-const BooksModel = require('../models/admin/books.model');
-const CategoriesModel = require('../models/admin/categories.model');
-const CartModel = require('../models/cart.model');
-const BalanceModel = require('../models/balance.model');
+const BooksModel = require("../models/admin/books.model");
+const CategoriesModel = require("../models/admin/categories.model");
+const CartModel = require("../models/cart.model");
+const BalanceModel = require("../models/balance.model");
 
 const homeController = {
-	show: async (req, res, next) => {
-		try {
-			let newArrivalBooks = await BooksModel.getAll();
-			if (newArrivalBooks?.length > 0) {
-				newArrivalBooks = newArrivalBooks.slice(0, 4);
-			}
-			const categories = await CategoriesModel.getAll();
-			const user = req.session.user;
-			if (user) {
-				const cart = await CartModel.getCartByCustomerId(user._id);
-				const totalQuantity = cart.reduce(
-					(total, item) => total + item.quantity,
-					0
-				);
-				req.session.cart = {
-					cart,
-					totalQuantity,
-				};
-			}
-			const balance = await BalanceModel.getBalance(user._id);
+  show: async (req, res, next) => {
+    try {
+      let newArrivalBooks = await BooksModel.getAll();
+      if (newArrivalBooks?.length > 0) {
+        newArrivalBooks = newArrivalBooks.slice(0, 4);
+      }
+      const categories = await CategoriesModel.getAll();
+      const user = req.session.user;
+      var balance;
+      if (user) {
+        const cart = await CartModel.getCartByCustomerId(user._id);
+        const totalQuantity = cart.reduce(
+          (total, item) => total + item.quantity,
+          0
+        );
+        req.session.cart = {
+          cart,
+          totalQuantity,
+        };
+        balance = await BalanceModel.getBalance(user._id);
+      }
 
-			res.render('home', {
-				title: 'Home',
-				success: req.flash('success'),
-				error: req.flash('error'),
-				newArrivalBooks,
-				categories,
-				amount: balance.amount,
-			});
-		} catch (error) {
-			next(error);
-		}
-	},
+      res.render("home", {
+        title: "Home",
+        success: req.flash("success"),
+        error: req.flash("error"),
+        newArrivalBooks,
+        categories,
+        amount: balance ? balance.amount : null,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
 module.exports = homeController;
