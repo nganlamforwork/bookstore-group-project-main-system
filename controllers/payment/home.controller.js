d9617c4d2b7346cf1412d1034a73aaa4961ba7f7
21127334 <lvlam21@clc.fitus.edu.vn>
2024-01-06 03:26:19 +0700

    feat: Update payment home page with fake data and header styling.

--- controllers/payment/home.controller.js
+++ controllers/payment/home.controller.js
@@ -1,7 +1,48 @@
 const homeController = {
   show: async (req, res, next) => {
+    const fakeData = [
+      {
+        id: 1,
+        activity: "Pay bookstore bill",
+        date: "January 5th, 2024",
+        amount: -1000,
+        status: "Success",
+      },
+      {
+        id: 2,
+        activity: "Pay bookstore bill",
+        date: "January 5th, 2024",
+        amount: -1000,
+        status: "Success",
+      },
+      {
+        id: 3,
+        activity: "Top-up Visa card",
+        date: "January 6th, 2024",
+        amount: 500,
+        status: "Success",
+      },
+      {
+        id: 4,
+        activity: "Top-up Visa card",
+        date: "January 6th, 2024",
+        amount: 500,
+        status: "Success",
+      },
+      {
+        id: 5,
+        activity: "Top-up Visa card",
+        date: "January 7th, 2024",
+        amount: 300,
+        status: "Success",
+      },
+    ];
     try {
-      res.redirect("/login");
+      res.render("payment/home", {
+        title: "Payment - Home",
+        layout: "payment",
+        transactions: fakeData,
+      });
     } catch (error) {
       next(error);
     }
