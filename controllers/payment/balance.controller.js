3037a2dac92703997e98a3dc177e5eccf54291d2
Louis2602 <trantunglam135790@gmail.com>
2024-01-10 23:53:29 +0700

    feat: Transfer money from user to admin payment account, adding withdraw for admin - auto subtracting current balance amount after withdraw money (no limit), display withdraw history as well as user payment

--- controllers/payment/balance.controller.js
+++ controllers/payment/balance.controller.js
@@ -1,91 +1,97 @@
-const CardModel = require("../../models/payment/cards.model");
-const PaymentHistoryModel = require("../../models/payment/history.model");
+const CardModel = require('../../models/payment/cards.model');
+const PaymentHistoryModel = require('../../models/payment/history.model');
 
 const BalanceController = {
-  displayRecharge: async (req, res, next) => {
-    try {
-      const userId = req.params.userId;
-      const balance = await CardModel.getBalance(userId);
-      res.render("payment/recharge", {
-        title: "Recharge Your Balance",
-        layout: "base",
-        balance: balance,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
-  rechargeBalance: async (req, res, next) => {
-    try {
-      const { cardholderName, cardNumber, expires, cvv, amount } = req.body;
+	displayRecharge: async (req, res, next) => {
+		try {
+			const userId = req.params.userId;
+			const user = req.session.user;
+			const balance = await CardModel.getBalance(userId);
+			res.render('payment/recharge', {
+				title: 'Recharge Your Balance',
+				layout: 'base',
+				balance: balance,
+				admin: user.role === 'superadmin',
+			});
+		} catch (error) {
+			next(error);
+		}
+	},
+	rechargeBalance: async (req, res, next) => {
+		try {
+			const { amount } = req.body;
+			const userId = req.params.userId;
 
-      const userId = req.params.userId;
+			// check if balance exists, refill
+			const existingBalance = await CardModel.getBalance(userId);
 
-      // check if balance exists, refill
-      const existingBalance = await CardModel.getBalance(userId);
-      var result;
-      if (existingBalance) {
-        // If the balance already exists, update its amount in the balance
-        const updatedAmount =
-          parseInt(existingBalance.amount) + parseInt(amount);
+			if (existingBalance) {
+				// If the balance already exists, update its amount in the balance
+				const updatedAmount =
+					parseInt(existingBalance.amount) + parseInt(amount);
 
-        result = await CardModel.updateBalance(
-          existingBalance._id,
-          updatedAmount
-        );
-        if (!result.acknowledged) {
-          await PaymentHistoryModel.add({
-            customerId: userId,
-            activity: "Top-up Visa card",
-            amount: amount,
-            income: true,
-            success: false,
-          });
-          res.status(400).send({
-            succes: false,
-            message: "Recharge balance failed",
-          });
-        }
-      } else {
-        result = await CardModel.rechargeBalance(
-          userId,
-          cardholderName,
-          cardNumber,
-          expires,
-          cvv,
-          parseInt(amount)
-        );
-        if (!result.acknowledged) {
-          await PaymentHistoryModel.add({
-            customerId: userId,
-            activity: "Top-up Visa card",
-            amount: amount,
-            income: true,
-            success: false,
-          });
-          res.status(400).send({
-            succes: false,
-            message: "Recharge balance failed",
-          });
-        }
-      }
-
-      await PaymentHistoryModel.add({
-        customerId: userId,
-        activity: "Top-up Visa card",
-        amount: amount,
-        income: true,
-        success: true,
-      });
-
-      res.status(200).send({
-        success: true,
-        message: "Recharge balance successfully",
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+				const result = await CardModel.updateBalance(
+					existingBalance._id,
+					updatedAmount
+				);
+				if (result.acknowledged) {
+					if (amount >= 0) {
+						await PaymentHistoryModel.add({
+							customerId: userId,
+							activity: 'Top-up Visa card',
+							amount: amount,
+							income: true,
+							success: true,
+						});
+						res.status(200).send({
+							success: true,
+							message: 'Recharge balance successfully',
+						});
+					} else {
+						await PaymentHistoryModel.add({
+							customerId: userId,
+							activity: 'Withdrawal',
+							amount: amount,
+							income: true,
+							success: true,
+						});
+						res.status(200).send({
+							success: true,
+							message: 'Withdraw money successfully',
+						});
+					}
+				} else {
+					if (amount > 0) {
+						await PaymentHistoryModel.add({
+							customerId: userId,
+							activity: 'Top-up Visa card',
+							amount: amount,
+							income: true,
+							success: false,
+						});
+						res.status(400).send({
+							succes: false,
+							message: 'Recharge balance failed',
+						});
+					} else {
+						await PaymentHistoryModel.add({
+							customerId: userId,
+							activity: 'Withdrawal',
+							amount: amount,
+							income: true,
+							success: false,
+						});
+						res.status(400).send({
+							succes: false,
+							message: 'Withdraw money failed',
+						});
+					}
+				}
+			}
+		} catch (error) {
+			next(error);
+		}
+	},
 };
 
 module.exports = BalanceController;
--- models/payment/cards.model.js
+++ models/payment/cards.model.js
@@ -1,37 +1,46 @@
-const db = require("../db");
-const schema = "payment_balances";
+const db = require('../db');
+const schema = 'payment_balances';
 
 const CardModel = {
-  updateCard: async (customerId, cardInfo) => {
-    try {
-      const result = await db.update(
-        schema,
-        "customerId",
-        customerId,
-        cardInfo
-      );
-      return result;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  getBalance: async (customerId) => {
-    try {
-      const balance = await db.get(schema, "customerId", customerId);
-      return balance;
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  updateBalance: async (balanceId, updatedBalance) => {
-    try {
-      const updateData = { amount: updatedBalance };
-      const result = await db.update(schema, "_id", balanceId, updateData);
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-  },
+	updateCard: async (customerId, cardInfo) => {
+		try {
+			const result = await db.update(
+				schema,
+				'customerId',
+				customerId,
+				cardInfo
+			);
+			return result;
+		} catch (err) {
+			console.error(err);
+		}
+	},
+	getBalance: async (customerId) => {
+		try {
+			const balance = await db.get(schema, 'customerId', customerId);
+			return balance;
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	updateBalance: async (balanceId, updatedBalance) => {
+		try {
+			const updateData = { amount: updatedBalance };
+			const result = await db.update(schema, '_id', balanceId, updateData);
+			console.log('🚀 ~ updateBalance: ~ updateData:', updateData);
+			return result;
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	getAdminBalance: async () => {
+		try {
+			const adminBalance = await db.get(schema, 'cardholderName', 'PAYMENT');
+			return adminBalance;
+		} catch (err) {
+			console.log(err);
+		}
+	},
 };
 
 module.exports = CardModel;
