7a6158d3627b451e4e885847f1511c76ef1fd1a1
Le Vu Ngan Lam <nganlamforwork@gmail.com>
2024-01-16 14:57:46 +0700

    feat: Update product category and home page layouts

--- public/js/category.js
+++ public/js/category.js
@@ -1,62 +1,60 @@
 const productsTemplateString = `
-{{#each books}}
-        <div class='col-3'>
-          <div class='book-card-container'>
-            <a href='/product/{{this._id}}'>
-
-              <div
-                class='img-container'
-                style="width: 100%;
-      padding-top: 150%;
-      background-image: url('{{this.thumbnail}}');
-      background-size: cover;"
-              >
+      {{#each books}}
+        <div class="col-3 book-card">
+          <a class="w-100" style="display: block;"href="/product/{{this._id}}">
+            <div class="img-container" style="width: 100%;
+                  padding-top: 150%;
+                  position: relative">
+              <div class="thumbnail" style="
+                position: absolute;
+                top: 0;
+                left:0;
+                right:0;
+                bottom:0;
+                  background-image: url('{{this.thumbnail}}');
+                  background-size: cover;"></div>
+              <div class="overlay fs-2">
+                <i class="fa-solid fa-eye"></i>
               </div>
-            </a>
-            <div class='card-body text-center py-4'>
-              <div
-                style='overflow: hidden; text-overflow: ellipsis; white-space: nowrap; text-overflow: ellipsis;'
-              >
-                <a href='/product/{{this._id}}'>
-                  <h1 class='fs-4 header-font fw-bold'>{{this.title}}</h1>
-                </a>
-              </div>
-              <span class='text-secondary fs-5'>$</span>
-              <span class='text-secondary fs-5'>{{this.price}}</span>
             </div>
-          </div>
+            <div class="card-body text-start py-4 text-center">
+              <p class="text-secondary m-0 text-uppercase">{{this.author_name}}</p>
+              <h1 class="fs-3 header-font fw-bold">{{this.title}}</h1>
+              <p class="text-secondary fs-5"><span>$</span>{{this.price}}</p>
+            </div>
+          </a>
         </div>
       {{/each}}
 `;
 
 function getParamsString() {
-  const filter = $('#filter-form').serialize();
+  const filter = $("#filter-form").serialize();
   const urlParams = new URLSearchParams(filter);
 
   return urlParams.toString();
 }
 
 function updateUrl(params) {
-  const newUrl = window.location.pathname + '?' + params;
-  history.pushState(null, '', newUrl);
+  const newUrl = window.location.pathname + "?" + params;
+  history.pushState(null, "", newUrl);
 }
 
 function handleFilter(e) {
   e.preventDefault();
   let params = getParamsString();
 
-  params = params + '&page=1'; // default page is 1
+  params = params + "&page=1"; // default page is 1
 
   $.ajax({
-    url: '/category/filter?' + params,
-    type: 'GET',
-    dataType: 'json',
+    url: "/category/filter?" + params,
+    type: "GET",
+    dataType: "json",
     success: function (data) {
       updateUrl(params);
       renderData(data);
     },
     error: function (error) {
-      console.log('Error:', error);
+      console.log("Error:", error);
     },
   });
 }
@@ -64,7 +62,7 @@ function handleFilter(e) {
 function renderData(data) {
   var productsTemplate = Handlebars.compile(productsTemplateString);
   var productsHtml = productsTemplate(data);
-  document.getElementById('book-cards-container').innerHTML = productsHtml;
+  document.getElementById("book-cards-container").innerHTML = productsHtml;
 
   const paginationTemplateString = `
   <ul class="pagination">
@@ -76,12 +74,12 @@ function renderData(data) {
   </ul>
 `;
 
-  Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
     return arg1 === arg2 ? options.fn(this) : options.inverse(this);
   });
 
-  Handlebars.registerHelper('loopTill', function (totalPages, options) {
-    let result = '';
+  Handlebars.registerHelper("loopTill", function (totalPages, options) {
+    let result = "";
     for (let i = 1; i <= totalPages; i++) {
       result += options.fn({ index: i });
     }
@@ -90,24 +88,24 @@ function renderData(data) {
 
   var paginationTemplate = Handlebars.compile(paginationTemplateString);
   var paginationHtml = paginationTemplate(data);
-  document.getElementById('paginationWrapper').innerHTML = paginationHtml;
+  document.getElementById("paginationWrapper").innerHTML = paginationHtml;
 }
 
 function paging(page) {
   let params = getParamsString();
 
-  params = params + '&page=' + page;
+  params = params + "&page=" + page;
 
   $.ajax({
-    url: '/category/filter?' + params,
-    type: 'GET',
-    dataType: 'json',
+    url: "/category/filter?" + params,
+    type: "GET",
+    dataType: "json",
     success: function (data) {
       updateUrl(params);
       renderData(data);
     },
     error: function (error) {
-      console.log('Error:', error);
+      console.log("Error:", error);
     },
   });
 }
